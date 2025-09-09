a294afbc9fa3451fa5ae811b9d2cf877026de0f0
ASANI <dezmant03@gmail.com>
2025-09-09 12:32:15 +0300

    Fourth Commit and Push, Second Collecion final

--- src/Second_collection/Solution_8_2.java
+++ src/Second_collection/Solution_8_2.java
@@ -1,4 +1,14 @@
 package Second_collection;
+import java.util.Scanner;
+// Чтение чисел
 
 public class Solution_8_2 {
+    public static void main(String[] args) {
+        Scanner scan =  new Scanner(System.in);
+        int g = scan.nextInt();
+        int h = scan.nextInt();
+        int p = scan.nextInt();
+        int result = (g+h+p)/3;
+        System.out.println(result);
+    }
 }
